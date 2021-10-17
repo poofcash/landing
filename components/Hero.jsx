18baae29405e2def0288abb2e7c17c5622087345
Brian Li <brnli7@gmail.com>
2021-10-17 06:00:42 -0700

    Update copies on landing page

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,19 +1,50 @@
-import { Box, Container, Heading, Text, Flex, Button, Image } from "@theme-ui/components";
+import {
+  Box,
+  Container,
+  Heading,
+  Text,
+  Flex,
+  Button,
+  Image,
+} from "@theme-ui/components";
 import React from "react";
 
 export const Hero = () => {
-    return (
-        <Flex sx={{ mt: [0, 2, 5, 5], pt: ['72px', 0, 0, 0], justifyContent: 'space-between', alignItems: ['center', 'flex-start', 'center', 'center'], flexDirection: ['column-reverse', 'column-reverse', 'row', 'row'] }}>
-            <Box sx={{ width: ['100%', '65%', '45%', '40%'] }}>
-                <Heading as="h1" sx={{ variant: 'text.display' }}>Now they see it, now they don’t</Heading>
-                <Box sx={{ pt: 3, width: ['100%', '100%', '80%', '90%'] }}>
-                    <Text>Poof is a decentralized protocol that keeps your financial data safe on CELO. It supports private transactions through mixing transfers in the fund pools. Play your cards close to your chest.</Text>
-                </Box>
-                <Button onClick={() => {
-                    window.open("https://app.poof.cash", "_blank");
-                }} mt="4">Launch App</Button>
-            </Box>
-            <Image src="images/Main.svg" sx={{ width: ['100%', '60%', '550px', '600px'], mb: [3, 3, 0, 0] }}></Image>
-        </Flex >
-    );
+  return (
+    <Flex
+      sx={{
+        mt: [0, 2, 5, 5],
+        pt: ["72px", 0, 0, 0],
+        justifyContent: "space-between",
+        alignItems: ["center", "flex-start", "center", "center"],
+        flexDirection: ["column-reverse", "column-reverse", "row", "row"],
+      }}
+    >
+      <Box sx={{ width: ["100%", "65%", "45%", "40%"] }}>
+        <Heading as="h1" sx={{ variant: "text.display" }}>
+          Now they see it, now they don’t
+        </Heading>
+        <Box sx={{ pt: 3, width: ["100%", "100%", "80%", "90%"] }}>
+          <Text>
+            Poof is a decentralized, private DeFi protocol. Users can privately
+            lend, borrow, send, and request crypto. Now available on Celo and
+            Fantom.
+          </Text>
+        </Box>
+        <Button
+          onClick={() => {
+            window.open("https://app.poof.cash", "_blank");
+          }}
+          mt="4"
+        >
+          Launch App
+        </Button>
+      </Box>
+      <Image
+        src="images/Main.svg"
+        sx={{ width: ["100%", "60%", "550px", "600px"], mb: [3, 3, 0, 0] }}
+      ></Image>
+    </Flex>
+  );
 };
+
--- components/Start.jsx
+++ components/Start.jsx
@@ -2,54 +2,117 @@ import { Box, Heading, Text, Flex, Image } from "@theme-ui/components";
 import React from "react";
 
 export const Start = () => {
-    return (
-        <div>
-            <Flex id="start" sx={{ mb: 4, mt: [5, 2, 5, 6], pt: [4, 2, 6, 6], textAlign: ['left', 'center'], flexDirection: 'column' }}>
-                <Heading as="h1" sx={{ variant: 'text.title' }}>Getting Started</Heading>
-                <Heading as="h2" sx={{ variant: 'text.subtitle' }}>Easier than learning how to shuffle cards</Heading>
-            </Flex>
-            <Flex sx={{ display: ['flex', 'none'], flexDirection: 'column' }}>
-                <Box mb="5">
-                    <Image src="images/Deposit.svg" sx={{ mb: 3, width: '100%' }}></Image>
-                    <Heading as="h3" sx={{ variant: 'text.h3' }}>Deposit</Heading>
-                    <Text>Safely and securely deposit rCELO into Poof and receive a private magic password for the transaction.</Text>
-                </Box>
-                <Box mb="5">
-                    <Image src="images/Wait.svg" sx={{ mb: 3, width: '100%' }}></Image>
-                    <Heading as="h3" sx={{ variant: 'text.h3' }}>Wait</Heading>
-                    <Text>Simply wait to increase privacy. The more deposits after yours, the better the privacy for your transaction.   </Text>
-                </Box>
-                <Box mb="5">
-                    <Image src="images/Earn.svg" sx={{ mb: 3, width: '100%' }}></Image>
-                    <Heading as="h3" sx={{ variant: 'text.h3' }}>Earn</Heading>
-                    <Text>Get rewarded for your patience. You earn anonymity points as your deposit sits in the fund pools.</Text>
-                </Box>
-                <Box>
-                    <Image src="images/Withdraw.svg" sx={{ mb: 3, width: '100%' }}></Image>
-                    <Heading as="h3" sx={{ variant: 'text.h3' }}>Withdraw</Heading>
-                    <Text>Whenever you’re ready, easily send money or withdraw your funds with the magic password.</Text>
-                </Box>
-            </Flex>
-            <Image sx={{ display: ['none', 'none', 'flex', 'flex'] }} src="images/Start.svg"></Image>
-            <Flex sx={{ flexDirection: 'row', display: ['none', 'flex'], width: '100%', justifyContent: 'space-between', mb: 6 }}>
-                <Box sx={{ display: ['none', 'block'], width: '22%' }}>
-                    <Heading as="h3" sx={{ variant: 'text.h3' }}>Deposit</Heading>
-                    <Text>Safely and securely deposit rCELO into Poof and receive a private magic password for the transaction.</Text>
-                </Box>
-                <Box sx={{ display: ['none', 'block'], width: '22%' }}>
-                    <Heading as="h3" sx={{ variant: 'text.h3' }}>Wait</Heading>
-                    <Text>Simply wait to increase privacy. The more deposits after yours, the better the privacy for your transaction.   </Text>
-                </Box>
-                <Box sx={{ display: ['none', 'block'], width: '22%' }}>
-                    <Heading as="h3" sx={{ variant: 'text.h3' }}>Earn</Heading>
-                    <Text>Get rewarded for your patience. You earn anonymity points as your deposit sits in the fund pools.</Text>
-                </Box>
-                <Box sx={{ display: ['none', 'block'], width: '22%' }}>
-                    <Heading as="h3" sx={{ variant: 'text.h3' }}>Withdraw</Heading>
-                    <Text>Whenever you’re ready, easily send money or withdraw your funds with the magic password.</Text>
-                </Box>
-            </Flex>
-        </div>
-
-    );
+  return (
+    <div>
+      <Flex
+        id="start"
+        sx={{
+          mb: 4,
+          mt: [5, 2, 5, 6],
+          pt: [4, 2, 6, 6],
+          textAlign: ["left", "center"],
+          flexDirection: "column",
+        }}
+      >
+        <Heading as="h1" sx={{ variant: "text.title" }}>
+          Getting Started
+        </Heading>
+        <Heading as="h2" sx={{ variant: "text.subtitle" }}>
+          Easier than learning how to shuffle cards
+        </Heading>
+      </Flex>
+      <Flex sx={{ display: ["flex", "none"], flexDirection: "column" }}>
+        <Box mb="5">
+          <Image src="images/Deposit.svg" sx={{ mb: 3, width: "100%" }}></Image>
+          <Heading as="h3" sx={{ variant: "text.h3" }}>
+            Deposit
+          </Heading>
+          <Text>
+            Deposit crypto into the Poof private savings account. While
+            deposited, no one can see your balance except for you.
+          </Text>
+        </Box>
+        <Box mb="5">
+          <Image src="images/Wait.svg" sx={{ mb: 3, width: "100%" }}></Image>
+          <Heading as="h3" sx={{ variant: "text.h3" }}>
+            Wait
+          </Heading>
+          <Text>
+            Simply wait to increase privacy. The more transactions after your
+            deposit, the better the privacy for your transaction.
+          </Text>
+        </Box>
+        <Box mb="5">
+          <Image src="images/Earn.svg" sx={{ mb: 3, width: "100%" }}></Image>
+          <Heading as="h3" sx={{ variant: "text.h3" }}>
+            Earn
+          </Heading>
+          <Text>
+            Automatically earn interest while deposited into Poof via lending
+            APR. You can earn additional yield at no cost by taking out a 0%
+            interest pToken loan.
+          </Text>
+        </Box>
+        <Box>
+          <Image
+            src="images/Withdraw.svg"
+            sx={{ mb: 3, width: "100%" }}
+          ></Image>
+          <Heading as="h3" sx={{ variant: "text.h3" }}>
+            Transfer
+          </Heading>
+          <Text>Transfer crypto privately in any amount to any user.</Text>
+        </Box>
+      </Flex>
+      <Image
+        sx={{ display: ["none", "none", "flex", "flex"] }}
+        src="images/Start.svg"
+      ></Image>
+      <Flex
+        sx={{
+          flexDirection: "row",
+          display: ["none", "flex"],
+          width: "100%",
+          justifyContent: "space-between",
+          mb: 6,
+        }}
+      >
+        <Box sx={{ display: ["none", "block"], width: "22%" }}>
+          <Heading as="h3" sx={{ variant: "text.h3" }}>
+            Deposit
+          </Heading>
+          <Text>
+            Deposit crypto into the Poof private savings account. While
+            deposited, no one can see your balance except for you.
+          </Text>
+        </Box>
+        <Box sx={{ display: ["none", "block"], width: "22%" }}>
+          <Heading as="h3" sx={{ variant: "text.h3" }}>
+            Wait
+          </Heading>
+          <Text>
+            Simply wait to increase privacy. The more transactions after your
+            deposit, the better the privacy for your transaction.
+          </Text>
+        </Box>
+        <Box sx={{ display: ["none", "block"], width: "22%" }}>
+          <Heading as="h3" sx={{ variant: "text.h3" }}>
+            Earn
+          </Heading>
+          <Text>
+            Automatically earn interest while deposited into Poof via lending
+            APR. You can earn additional yield at no cost by taking out a 0%
+            interest pToken loan.
+          </Text>
+        </Box>
+        <Box sx={{ display: ["none", "block"], width: "22%" }}>
+          <Heading as="h3" sx={{ variant: "text.h3" }}>
+            Withdraw
+          </Heading>
+          <Text>Transfer crypto privately in any amount to any user.</Text>
+        </Box>
+      </Flex>
+    </div>
+  );
 };
+
